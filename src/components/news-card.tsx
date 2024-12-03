14cb89a9718f07919d200444e053b8f003a0f6fa
Kush <85061303+kush992@users.noreply.github.com>
2024-12-03 18:50:59 +0100

    light mode color for badge

--- src/components/news-card.tsx
+++ src/components/news-card.tsx
@@ -34,7 +34,7 @@ export function NewsCard({ article }: NewsCardProps) {
 				{article.category.map((cat) => (
 					<Badge
 						key={cat}
-						className='text-primary bg-accent inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2'
+						className='dark:text-primary dark:bg-accent inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2'
 					>
 						{cat}
 					</Badge>
